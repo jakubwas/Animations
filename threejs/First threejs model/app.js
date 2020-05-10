67e1585b031374d5bf8f78e47c38961b0c21ce69
Jakub Was <wasjakub3237@gmail.com>
2020-05-10 18:31:21 +0200

    Add OrbitControls , add comments

--- threejs/First threejs model/app.js
+++ threejs/First threejs model/app.js
@@ -1,8 +1,24 @@
-let scene, camera, renderer, cube;
+/* Declare elements */
+let scene, camera, renderer;
+let cube;
+
+/* Event Listeners */
+window.addEventListener("resize", onWindowResize, false);
+
+/* Event Listeners Functions */
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+/* Three.js Functions */
 
 function init() {
+    // Set up the scene
     scene = new THREE.Scene();
 
+    // Set up the camera
     camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
@@ -10,36 +26,39 @@ function init() {
         1000
     );
 
+    // Set up the renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
-
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    // Create Cube
+    const geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
     const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
     cube = new THREE.Mesh(geometry, material);
+
+    // Add cube to the scene
     scene.add(cube);
 
     camera.position.z = 5;
+    // camera.position.y = 5;
+    // camera.position.x = 5;
+
+    // cube.position.x = 2;
+    // cube.position.y = 2;
+    
+    // Add Orbit controls which allows the camera to orbit around a target
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.addEventListener("update", renderer);
+
+    animate();
 }
 
 function animate() {
     requestAnimationFrame(animate);
-
     cube.rotation.x += 0.015;
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.addEventListener("resize", onWindowResize, false);
-
 init();
-animate();
