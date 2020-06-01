6092a805d251bb4e8d1ca39ef785e4c825c5cdb5
Jakub Was <wasjakub3237@gmail.com>
2020-06-01 16:06:32 +0200

    Create basic svg icon animation with scrollmagic and gsap

--- gsap/SVG-drawing/app.js
+++ gsap/SVG-drawing/app.js
@@ -1,4 +1,19 @@
-var path = document.querySelector('.path');
-var length = path.getTotalLength();
+let path = document.querySelector(".path");
+let length = path.getTotalLength();
+console.log(length);
 
-console.log(length)
+let controller = new ScrollMagic.Controller();
+let scene = new ScrollMagic.Scene({
+    triggerElement: ".svg",
+})
+    .setTween(
+        gsap.to(".path", {
+            duration: 3,
+            ease: "power1.inOut",
+            immediateRender: false,
+            strokeDashoffset: 0,
+            fill: "rgba(152,152,152,0.72)",
+        })
+    )
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
