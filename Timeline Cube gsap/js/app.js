4ea5c80f0d91333d57d8c265c5cdd25dfac7740e
jakub.was <jakub.was@continentalbakeries.pl>
2021-01-11 18:37:20 +0100

    .

--- Timeline Cube gsap/js/app.js
+++ Timeline Cube gsap/js/app.js
@@ -78,47 +78,4 @@ tm.fromTo(
     .to(".box2", { x: "-=41", y: "+=50", duration: 2 })
     .to(".box1", { x: "+=41", y: "-=50", duration: 2 }, "-=2")
     .addLabel("border")
-    .to(".box2", {
-        borderLeft: "2px solid rgba(255, 255,255, 0)",
-        borderBottom: "1px solid rgba(255, 255,255, 0)",
-        duration: 1.5,
-        ease: "none",
-    })
-    .to(
-        ".box4",
-        {
-            borderTop: "2px solid rgba(255, 255,255, 0)",
-            borderLeft: "1px solid rgba(255, 255,255, 0)",
-            duration: 1.5,
-            ease: "none",
-        },
-        "border"
-    )
-    .to(
-        ".box5",
-        {
-            borderBottom: "2px solid rgba(255, 255,255, 0)",
-            borderRight: "1px solid rgba(255, 255,255, 0)",
-            duration: 1.5,
-            ease: "none",
-        },
-        "border"
-    )
-    .to(".box2, .box4, .box5", { opacity: 0 })
-    .addLabel("colors")
-    .to(
-        ".box1",
-        { backgroundColor: "#086972", duration: 5, delay: 1 },
-        "border"
-    )
-    .to(
-        ".box3",
-        { backgroundColor: "#071a52", duration: 5, delay: 1 },
-        "border"
-    )
-    .to(
-        ".box6",
-        { backgroundColor: "#17b978", duration: 5, delay: 1 },
-        "border"
-    )
-    .to(".box", { scale: 0.1, opacity: 0, duration: 4, delay: 1 });
+    
